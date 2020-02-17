8e7ffe00bfc180925442f057b9ad62054dd5caf9
Sumita parte <sm524163@dal.ca>
2020-02-17 16:44:51 -0400

    added factorial program

---
+++ factorial.py
@@ -0,0 +1,13 @@
+def factorial(num):
+  if num < 0:
+    print("Please enter positive numbers only.")
+  elif num == 0:
+   print("The factorial of 0 is 1.")  
+  else:
+   fact =1 
+   for i in range(1,num+1):
+       fact = fact*i
+   print("The factorial of",num,"is",fact)
+
+num = int(input("Enter your name : ")) 
+factorial(num)
